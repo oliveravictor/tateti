1510e6bed7bcfac0de1f9b26a890ff2e96cfa428
oliveravictor <ovictoriox@gmail.com>
2022-01-21 19:22:57 -0300

    create variables

---
+++ script.js
@@ -0,0 +1,6 @@
+let turn = 1;
+let piece = ["O", "X"];
+let shots = 0;
+let finishedGame = false;
+let win = document.getElementById("win");
+let buttons = Array.from(document.getElementsByTagName("button")); 
